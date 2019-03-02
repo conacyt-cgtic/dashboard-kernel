11cdd502e7fbf40f3105e7f0330a73669f5a5c36
garellano <garellano@mbp-de-gustavo>
2019-03-02 11:32:57 -0600

    ok

--- src/main/java/mx/conacyt/cgtic/dashboard/kernel/Calculator.java
+++ src/main/java/mx/conacyt/cgtic/dashboard/kernel/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     private final static Logger LOG = Logger.getLogger(Calculator.class);
     
     public long computeFactLen(int n) {
-        LOG.info("Calculating fact len for: "+n); 
+        LOG.info("Calculating fact len for:: "+n); 
         double res = 1.0;
         for(int i=1; i<=n; i++)
             res = res + Math.log10(i);
