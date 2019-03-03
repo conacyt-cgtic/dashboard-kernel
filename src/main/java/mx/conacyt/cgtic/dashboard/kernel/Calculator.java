ba7e31e614b996371dffa67dbf7ad77c43401225
garellano <garellano@mbp-de-gustavo>
2019-03-02 20:05:10 -0600

    adding comments javadoc

--- src/main/java/mx/conacyt/cgtic/dashboard/kernel/Calculator.java
+++ src/main/java/mx/conacyt/cgtic/dashboard/kernel/Calculator.java
@@ -2,9 +2,21 @@ package mx.conacyt.cgtic.dashboard.kernel;
 
 import org.apache.log4j.Logger;
 
+/**
+ * abc
+ * 
+ * @author garellano
+ *
+ */
 public class Calculator {
     private final static Logger LOG = Logger.getLogger(Calculator.class);
     
+    /**
+     * xyz
+     * 
+     * @param n
+     * @return
+     */
     public long computeFactLen(int n) {
         LOG.info("Calculating fact len for:: "+n); 
         double res = 1.0;
